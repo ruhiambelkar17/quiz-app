2cc63d0e3c3eb333b2d9e0cdf9eef8e18f97b3c8
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-13 01:50:32 +0100

    updated

--- quiz-app/src/views/Home.js
+++ quiz-app/src/views/Home.js
@@ -5,10 +5,11 @@ function Home() {
     return (
       <div style={{backgroundColor: '#F4FFFE',height:'918px'}}>
         <Navbar />
-        <div className='d-flex justify-content-center mt-3'>
+        <div className='d-flex justify-content-center mt-3 mb-3'>
             
             <img src={Img1} width='300' height='350'  alt='img'/>
         </div>
+        <h3 className='text-center'> No worries, practice,understand and improve your knoweldge by taking quiz!!</h3>
       </div>
     );
   }
