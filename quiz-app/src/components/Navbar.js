abeda3a22724df8dc4f0bf62c10745ba63b360ed
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-16 00:23:35 +0100

    updated route for home page

--- quiz-app/src/components/Navbar.js
+++ quiz-app/src/components/Navbar.js
@@ -1,41 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (           
-            <nav class="navbar navbar-expand-lg bg-body-tertiary shadow ">
-                <div class="container-fluid">
-                <a class="navbar-brand ms-5 text-style" href="#link"
-                    >QuizVerse</a
-                >
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary shadow ">
+      <div className="container-fluid">
+        <Link to="/">
+          <p className="navbar-brand ms-5 text-style">QuizVerse</p>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <ul class="navbar-nav justify-content-end me-5">
-                    <li class="nav-item p-3">
-                    <a class="nav-link" href="#link" >Sources</a>
-                    </li>
-                    
-                    <li class="nav-item p-3">
-                    <a class="nav-link" href="#link" >About</a>
-                    </li>
-                    <li class="nav-item p-3">
-                    <a class="nav-link" href="#link">Contact</a>
-                    </li>
-                </ul>
-                </div>
-            </nav>
-        
-        )
-    
+        <ul className="navbar-nav justify-content-end me-5">
+          <li className="nav-item p-3">
+            <a className="nav-link" href="#link">
+              Sources
+            </a>
+          </li>
+
+          <li className="nav-item p-3">
+            <a className="nav-link" href="#link">
+              About
+            </a>
+          </li>
+          <li className="nav-item p-3">
+            <a className="nav-link" href="#link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
