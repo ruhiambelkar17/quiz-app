e3076d06eeceb9aa7236197b9d2badf507bc2b5e
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-15 12:31:44 +0100

    updated router paths

--- quiz-app/src/router/index.js
+++ quiz-app/src/router/index.js
@@ -3,16 +3,22 @@ import { createBrowserRouter } from "react-router-dom";
 // views
 import AboutUs from "../views/AboutUs";
 import GetById from "../views/GetById";
+import FrontEnd from "../views/FrontEnd";
+import Home from "../views/Home"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
   },
 
   {
     path: "/about-us",
     element: <AboutUs />,
+  },
+  {
+    path: "/front-end-development",
+    element: <FrontEnd />,
   },
 
   {
