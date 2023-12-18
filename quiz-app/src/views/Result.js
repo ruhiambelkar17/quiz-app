abb33410ca9c74f47417ef399aa85850b0f0ccef
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-18 23:49:46 +0100

    updated ui of result

--- quiz-app/src/views/Result.js
+++ quiz-app/src/views/Result.js
@@ -1,9 +1,14 @@
 function Result (){
     return (
         <div>
-            <div className="card">
-                Your score:18/20
-            </div>
+           <div className="card">
+ 
+  <div className="card-body">
+    <h5 className="card-title">Thanks for taking an Assessment!!</h5>
+    <p className="card-text">Your Score: 18/20</p>
+    <a href="#" className="btn btn-primary" style={{ backgroundColor: "#056D61", border: "#056D61" }}>Home</a>
+  </div>
+</div>
         </div>
     );
 }
