971652dd12ca506d8e2fe4471d17a76700976a6e
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-24 01:56:51 +0100

    updated

--- quiz-app/src/components/Timer.js
+++ quiz-app/src/components/Timer.js
@@ -11,15 +11,16 @@ function Timer(props){
 
     function updateTimer(){
         let a=props.nextPg ;
-        if (a>0){
-            a > 0 && setTimeout(() => setTimer(a - 1), 1000);
-            props.onUpdateTimer(a);
+
+        if (a){
+            setTimer(60);
             }
+            console.log("******",a)
             
-            else {
+        
                 timerValue > 0 && setTimeout(() => setTimer(timerValue - 1), 1000);
                 props.onUpdateTimer(timerValue);
-            }
+            
             //props.onUpdateTimer(timerValue);
         //timerValue > 0 && setTimeout(() => setTimer(timerValue - 1), 1000);
         // if (props.nextPg){
@@ -38,7 +39,7 @@ function Timer(props){
 
     return(
         <div>
-            <span>{timerValue} or {props.nextPg}</span>
+            <span>{timerValue} or {props.nextPg} </span>
         </div>
     )
 }
