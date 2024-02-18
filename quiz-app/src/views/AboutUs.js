25e38c7de832100c0afefd5bc70c78ed64287db9
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-02-18 22:58:28 +0100

    updated ui

--- quiz-app/src/views/AboutUs.js
+++ quiz-app/src/views/AboutUs.js
@@ -1,11 +1,15 @@
-
+import "./Styles/AboutUs.css";
 function AboutUs (){
 
     return (
         <>
-            <div style={{height:"500px",background: "#056D61",
+        kkkk
+            {/* <div style={{height:"500px",background: "#056D61",
 background: "linear-gradient(141deg, rgba(2,0,36,1) 0%, rgba(5,109,97,1) 35%, rgba(0,212,255,1) 100%)"}}>
 
+            </div> */}
+            <div className="box">
+hi
             </div>
         </>
     )
