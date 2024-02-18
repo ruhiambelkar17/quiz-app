f99a61169cadc2fcea40b5677255da707beda08c
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-02-18 23:10:55 +0100

    updated ui of about

--- quiz-app/src/views/AboutUs.js
+++ quiz-app/src/views/AboutUs.js
@@ -1,15 +1,13 @@
+import Navbar from "../components/Navbar";
 import "./Styles/AboutUs.css";
 function AboutUs (){
 
     return (
         <>
-        kkkk
-            {/* <div style={{height:"500px",background: "#056D61",
-background: "linear-gradient(141deg, rgba(2,0,36,1) 0%, rgba(5,109,97,1) 35%, rgba(0,212,255,1) 100%)"}}>
-
-            </div> */}
-            <div className="box">
-hi
+        <Navbar />
+            
+            <div className="box d-flex justify-content-center ">
+                <p className="">It's time to know about us!!</p>
             </div>
         </>
     )
