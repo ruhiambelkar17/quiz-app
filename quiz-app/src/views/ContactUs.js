706543844a51057ccab0add04c89b8a9981d9824
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-02-18 22:29:02 +0100

    updated functionality for contact us

--- quiz-app/src/views/ContactUs.js
+++ quiz-app/src/views/ContactUs.js
@@ -1,26 +1,76 @@
-export default function ContactUs(){
-    return (
-        <>
-        <div className="m-5">
-            <h3>Contact Us</h3>
-            <form>
-                <div className="mb-3">
-                    <label className="form-label" for="name" > Name</label>
-                    <input className="form-control" type="text" id="name" required />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label" for="name" > E-mail</label>
-                    <input className="form-control" type="text" id="name" required />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label" for="name" > Your message</label>
-                    <textarea className="form-control" type="text" id="name" required/>
-                </div>
-                <div>
-                    <button className="btn btn-primary"> Submit</button>
-                </div>
-            </form>
-        </div>
-        </>
-    )
+import { useState } from "react";
+
+export default function ContactUs() {
+  const [inputs, setInputs] = useState([
+    { username: "", email: "", message: "" },
+  ]);
+
+  const handleChange = (event, index) => {
+    let { name, value } = event.target;
+    let onChangeValue = [...inputs];
+    console.log(onChangeValue);
+    onChangeValue[index][name] = value;
+    setInputs(onChangeValue);
+  };
+
+  const SubmitMessage = () => {
+    setInputs([...inputs, { username: "", email: "", message: "" }]);
+    console.log("**users", inputs);
+  };
+  return (
+    <>
+      <div className="m-5">
+        <h3>Contact Us</h3>
+       {inputs.map((item,index)=>(
+         <form key={index}>
+         <div className="mb-3">
+           <label className="form-label" for="name">
+             {" "}
+             User Name
+           </label>
+           <input
+             className="form-control"
+             type="text"
+             name="username"
+             value={item.username}
+             onChange={(event) => handleChange(event, index)}
+             required
+           />
+         </div>
+         <div className="mb-3">
+           <label className="form-label" for="email">
+             {" "}
+             E-mail
+           </label>
+           <input
+             className="form-control"
+             type="text"
+             name="email"
+             value={item.email}
+             onChange={(event) => handleChange(event, index)}
+             required
+           />
+         </div>
+         <div className="mb-3">
+           <label className="form-label" for="message">
+             {" "}
+             Your message
+           </label>
+           <textarea
+             className="form-control"
+             type="text"
+             name="message"
+             value={item.message}
+             onChange={(event) => handleChange(event, index)}
+             required
+           />
+         </div>
+         <div>
+           <button className="btn btn-primary" onClick={SubmitMessage}> Submit</button>
+         </div>
+       </form>
+       ))}
+      </div>
+    </>
+  );
 }
