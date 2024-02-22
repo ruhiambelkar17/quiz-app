56510006631773aa2e0e76b5114ca761f55f2fe9
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-02-22 20:03:03 +0100

    updated home ui

--- quiz-app/src/views/Home.js
+++ quiz-app/src/views/Home.js
@@ -2,68 +2,8 @@ import Navbar from "../components/Navbar";
 import Img1 from "../assets/images/study.png";
 import { Link } from "react-router-dom";
 //import FrontEnd from "./FrontEnd";
-function DomainCard(props){
-    return (
-        <div>
-            <Link to={props.pathName}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="300"
-                height="250"
-                viewBox="0 0 304 271"
-                fill="none"
-              >
-                <g filter="url(#filter0_d_20_17)">
-                  <path
-                    d="M15.7778 17.7778C15.7778 17.7778 280.778 -22.2222 280.778 17.7778C280.778 57.7778 300.278 275.278 280.778 252.778C261.278 230.277 51.2778 267.777 15.7778 252.778C-19.7222 237.778 15.7778 17.7778 15.7778 17.7778Z"
-                    fill="#B5FDF6"
-                  />
-                </g>
-                <defs>
-                  <filter
-                    id="filter0_d_20_17"
-                    x="0"
-                    y="0"
-                    width="303.444"
-                    height="270.432"
-                    filterUnits="userSpaceOnUse"
-                    color-interpolation-filters="sRGB"
-                  >
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                    <feColorMatrix
-                      in="SourceAlpha"
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                      result="hardAlpha"
-                    />
-                    <feOffset dx="10" dy="10" />
-                    <feGaussianBlur stdDeviation="2" />
-                    <feComposite in2="hardAlpha" operator="out" />
-                    <feColorMatrix
-                      type="matrix"
-                      values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                    />
-                    <feBlend
-                      mode="normal"
-                      in2="BackgroundImageFix"
-                      result="effect1_dropShadow_20_17"
-                    />
-                    <feBlend
-                      mode="normal"
-                      in="SourceGraphic"
-                      in2="effect1_dropShadow_20_17"
-                      result="shape"
-                    />
-                  </filter>
-                </defs>
-                <text x="50" y="130" fill="#056D61">
-                  {props.name}
-                </text>
-              </svg>
-            </Link>
-          </div>
-    )
-}
+import "./Styles/Home.css";
+
 
 function Home() {
 //   const cardStyle = {
@@ -91,23 +31,23 @@ function Home() {
 
   const count=1;
   return (
-    <div style={{ backgroundColor: "#F4FFFE", height: "918px" }}>
+    <div style={{  height: "918px" }}>
       <Navbar />
-      <h1>counter:{count}</h1>
-      <button >increment</button>
+     
       <div className="d-flex justify-content-center mt-3 mb-3">
-        <img src={Img1} width="300" height="350" alt="img" />
+        <img src={Img1} width="300" height="350" alt="img" className="" />
       </div>
       <h3 className="text-center">
         Practice,understand and improve your knoweldge with QuizVerse!!
       </h3>
-      <div className="row d-flex justify-content-center mt-5">
-        <div className="col-md-3 d-flex justify-content-center">
-         {/* { quizDomains.map(DomainCard)} */}
-         {quizDomains.map(item=>{
-            return <DomainCard name={item.name} pathName={item.pathName} key={item.name} />
-          })}
-        </div>
+      <div className="row d-flex justify-content-center mt-5 mx-1">
+        {quizDomains.map((item,index)=>(
+          <div className="col-md-3 rounded me-5 d-flex justify-content-center align-items-center shadow" style={{backgroundColor:"#056D61", height:"20vh"}}>
+          <a href="#" > <Link className="link" to={item.pathName}>
+                {item.name}</Link>
+                </a>
+          </div>
+        ))}
       </div>
     </div>
   );
